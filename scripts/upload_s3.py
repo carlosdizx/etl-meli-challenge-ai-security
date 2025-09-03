f327d65b209be7d457f1fa9fa5ef9870e518245f
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2025-09-02 23:25:11 -0500

    :sparkles: set files with metadata

--- scripts/upload_s3.py
+++ scripts/upload_s3.py
@@ -30,14 +30,37 @@ for file in files_to_upload:
         raise Exception(f"No se encontraron {count} archivos para subir.")
 
 
+def get_content_type_and_disposition(file_path):
+    print(f"Procesando {file_path}")
+    if file_path.endswith(".json"):
+        return "application/json", "inline"
+    elif file_path.endswith(".csv"):
+        return "text/csv", "inline"
+    elif file_path.endswith(".pkl"):
+        return "application/octet-stream", "attachment"
+    else:
+        return "application/octet-stream", "attachment"
+
+
 def upload_files_to_s3(files):
     for file_path in files:
         file_name = os.path.basename(file_path)
-        s3_file_path = os.path.join(file_name)
+        s3_file_path = os.path.join("etl", file_name)
+
+        content_type, content_disposition = get_content_type_and_disposition(str(file_path))
 
         try:
             print(f"Subiendo {file_name} a S3...")
-            s3_client.upload_file(str(file_path), secrets["AWS_S3_BUCKET"], s3_file_path)
+
+            s3_client.upload_file(
+                str(file_path),
+                secrets["AWS_S3_BUCKET"],
+                s3_file_path,
+                ExtraArgs={
+                    "ContentType": content_type,
+                    "ContentDisposition": content_disposition
+                }
+            )
             print(f"✅ Archivo {file_name} subido exitosamente.")
         except Exception as e:
             print(f"❌ Error al subir {file_name} a S3: {e}")
